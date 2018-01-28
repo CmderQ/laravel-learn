eb2d70c03ba38417d1641ac66461ac5ed5a7834f
cmderQ <1257698043@qq.com>
2018-01-28 19:08:12 +0800

    路由设置

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,5 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('student/index', ['uses' => 'StudentController@index']);
+Route::group([], function (){
+    Route::get('student/index', ['uses' => 'StudentController@index']);
+    Route::any('student/create', ['uses' => 'StudentController@create']);
+    Route::any('student/save', ['uses' => 'StudentController@save']);
+    Route::any('student/update/{id}', ['uses' => 'StudentController@update']);
+    Route::any('student/detail/{id}', ['uses' => 'StudentController@detail']);
+    Route::any('student/delete/{id}', ['uses' => 'StudentController@delete']);
+});
+
 
